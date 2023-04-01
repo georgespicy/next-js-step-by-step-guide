37549c5abce80a8b6de680965c204e4d406bb02d
georgespicy <kolawolegeorge28743@gmail.com>
2023-04-01 12:22:31 +0100

    added loading page

---
+++ app/about/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <h1>loading ..................................................................................... </h1>
+  )
+}
+
+export default loading
